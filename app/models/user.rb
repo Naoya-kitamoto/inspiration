5e09024ce51b87d8177de5776a3462e46a6f404c
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-09-05 16:14:38 +0900

    新規登録画面の成形

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+         
+         validates :nickname,         presence: true
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'cannot use half width character' } do
+           validates :last_name,        presence: true,
+                                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'cannot use half width character' }
+           validates :first_name,       presence: true,
+                                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'cannot use half width character' }
+         end
+         validates :last_name_kana, presence: true,
+                                    format: { with: /\A[\p{katakana}ー－]+\z/ }
+         validates :first_name_kana, presence: true,
+                                     format: { with: /\A[\p{katakana}ー－]+\z/ }
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'must include at least one letter and one number'
+         has_many :comments
+         has_many :ideas
+        end
