fae875ca793c6129b2ac68a83054eaeedaa0314e
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-09-06 14:05:02 +0900

    ビューを整える

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,23 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all
   end
+
+  def new
+    @idea = Idea.new
+  end
+
+    def create
+      @idea = Idea.new(idea_params)
+      if @idea.save
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    private
+
+    def idea_params
+      params.require(:idea).permit(:title, :subject,:category_id).merge(user_id: current_user.id)
+    end
 end
