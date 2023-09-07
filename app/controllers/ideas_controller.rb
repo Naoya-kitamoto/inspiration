aaf552b91cd13bcc0587aab03f24928587ffe010
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-09-07 16:44:50 +0900

    非ログインユーザーの投稿画面の遷移をさせない

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,5 @@
 class IdeasController < ApplicationController
+  before_action :move_to_log_in, only: :new
   def index
     @ideas = Idea.includes(:user).all
     @idea = Idea.new
@@ -22,4 +23,11 @@ class IdeasController < ApplicationController
     def idea_params
       params.require(:idea).permit(:title, :category_id, :subject, :image).merge(user_id: current_user.id)
     end
+
+    def move_to_log_in
+      return if user_signed_in?
+  
+      redirect_to new_user_session_path
+    end
+    
 end
