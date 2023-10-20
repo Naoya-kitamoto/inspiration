a2056711d6686dab01d1d746b803a27466dd8dde
Naoya-kitamoto <k_naoya_1028@ezweb.ne.jp>
2023-10-21 03:07:57 +0900

    バリデーションの設定とコメントコントローラーの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to idea_path(@comment.idea)
+    else
+      @idea = @comment.idea
+      @comments = @idea.comments
+      render "ideas/show"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, idea_id: params[:idea_id])
+  end
 end
